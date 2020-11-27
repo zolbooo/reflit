8b1df7c77a1e9cf3822a9fe4e2ddcebde2a6dc3f
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-11-28 00:01:46 +0800

    feat: add image and fragment component

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,16 @@
+import { Fragment as ReactFragment } from 'react';
+import type { ReactNode } from 'react';
+
 import { reforwardJSX } from './utils';
 
 export const View = reforwardJSX<
   Omit<JSX.IntrinsicElements['div'], 'children'>
 >('div');
+
+export const Image = reforwardJSX<
+  Omit<JSX.IntrinsicElements['img'], 'children'>
+>('img');
+
+export const Fragment = (...children: ReactNode[]) => (
+  <ReactFragment>{children}</ReactFragment>
+);
