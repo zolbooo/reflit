9a875501670ec154ddffe50c56ce33f7799c283c
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-11-28 18:44:40 +0800

    feat: add list components

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,6 +4,13 @@ export const View = reforwardJSX<
   Omit<JSX.IntrinsicElements['div'], 'children'>
 >('div');
 
+export const List = reforwardJSX<Omit<JSX.IntrinsicElements['ul'], 'children'>>(
+  'ul',
+);
+export const OrderedList = reforwardJSX<
+  Omit<JSX.IntrinsicElements['ol'], 'children'>
+>('ol');
+
 export const Image = reforwardJSX<
   Omit<JSX.IntrinsicElements['img'], 'children'>
 >('img');
