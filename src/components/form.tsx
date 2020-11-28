d303a161126fba7a4ef45a866be2431b6afeb1bd
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-11-29 02:13:40 +0800

    feat: add form component

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,5 +1,9 @@
 import { reforwardJSX } from './utils';
 
+export const Form = reforwardJSX<
+  Omit<JSX.IntrinsicElements['form'], 'children'>
+>('form');
+
 export const Input = reforwardJSX<
   Omit<JSX.IntrinsicElements['input'], 'children'>
 >('input');
