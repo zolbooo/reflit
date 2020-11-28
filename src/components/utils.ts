6962622b13dd69d75eda5d3517955854b1b47495
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-11-28 18:36:46 +0800

    feat(dev): show elements in DevTools

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -1,7 +1,22 @@
-import { createElement } from 'react';
+import { forwardRef, createElement } from 'react';
 import type { ReactNode } from 'react';
 
 export function reforwardJSX<P>(element: string) {
+  if (process.env.NODE_ENV === 'development') {
+    // Create element factor callback for React DevTools
+    const fn = (
+      { children = [], ...props }: P & { children: ReactNode[] },
+      ref: any,
+    ) => createElement(element, { ...(props ?? {}), ref }, ...children);
+    // Set function name for DevTools
+    Object.defineProperty(fn, 'name', {
+      value: element,
+      writable: false,
+    });
+
+    return (props: P, ...children: ReactNode[]) =>
+      createElement(forwardRef(fn) as any, { ...props, children });
+  }
   return (props?: P, ...children: ReactNode[]) =>
     createElement(element, props ?? {}, ...children);
 }
